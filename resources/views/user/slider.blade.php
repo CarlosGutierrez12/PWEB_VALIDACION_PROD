815460370cec6953ef8a4ce8bbc903215b878047
CarlosGutierrez12 <carlos.gutierrez@gers.com>
2024-10-31 12:00:00 -0500

    Home page views added

---
+++ resources/views/user/slider.blade.php
@@ -0,0 +1,45 @@
+<section class="home-slider position-relative pt-50">
+    <div class="hero-slider-1 dot-style-1 dot-style-1-position-1">
+        <div class="single-hero-slider single-animation-wrap">
+            <div class="container">
+                <div class="row align-items-center slider-animated-1">
+                    <div class="col-lg-5 col-md-6">
+                        <div class="hero-slider-content-2">
+                            <h4 class="animated">Ofertas</h4>
+                            <h2 class="animated fw-900">Precios espectaculaes</h2>
+                            <h1 class="animated fw-900 text-brand">En todos los productos</h1>
+                            <p class="animated">Ahorra con cupones de hasta el 50</p>
+                            <a class="animated btn btn-brush btn-brush-3" href="{{route('user.shop')}}"> Comprar ahora </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-7 col-md-6">
+                        <div class="single-slider-img single-slider-img-1">
+                            <img class="animated slider-1-1" src="user/assets/imgs/slider/slider-01.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="single-hero-slider single-animation-wrap">
+            <div class="container">
+                <div class="row align-items-center slider-animated-1">
+                    <div class="col-lg-5 col-md-6">
+                        <div class="hero-slider-content-2">
+                            <h4 class="animated">Promociones</h4>
+                            <h2 class="animated fw-900">Lo último</h2>
+                            <h1 class="animated fw-900 text-7">Excelentes equipos</h1>
+                            <p class="animated">Ahorra con cupones de hasta el 20% en equipos electrónicos</p>
+                            <a class="animated btn btn-brush btn-brush-2" href="{{route('user.shop')}}"> Compra ahora </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-7 col-md-6">
+                        <div class="single-slider-img single-slider-img-1">
+                            <img class="animated slider-1-2" src="user/assets/imgs/slider/slider-2.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>                
+    </div>
+    <div class="slider-arrow hero-slider-1-arrow"></div>
+</section>
